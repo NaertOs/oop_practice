b637935381ecff64ee522384ffe17b7031bd4e31
Home PC <usoltsev57@gmail.com>
2024-10-30 21:57:07 +0700

    changes which i cant explain

---
+++ OOP/Classes/Garage.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Classes
+{
+    internal class Garage
+    {
+        public List<Vehicle> _vehicles;
+
+
+
+        public Garage()
+        {
+            _vehicles = new List<Vehicle>();
+        }
+
+        public void PrintListOfVehicles()
+        {
+            if (_vehicles.Count != 0)
+            {
+                int i = 1;
+                foreach (var vehicle in _vehicles)
+                {
+                    Console.WriteLine("{0}. {1}", i, vehicle.VehicleName);
+                    i++;
+                }
+            }
+        }
+
+        public object Vehicles
+        {
+            get { return _vehicles; }
+        }
+
+        public Vehicle GetVehicleByID(int vehicleNumber)
+        {
+            return _vehicles[vehicleNumber - 1];
+        }
+
+        public int GetVNumberOfVehicles()
+        {
+            return _vehicles.Count;
+        }
+
+        public void AddVehicle(Vehicle vehicle)
+        {
+            _vehicles.Add(vehicle);
+        }
+
+        public void RemoveVehicleByID(int vehicleNumber)
+        {
+            _vehicles.Remove(_vehicles[vehicleNumber-1]);
+        }
+    }
+}
--- OOP/Program.cs
+++ OOP/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
@@ -14,8 +15,17 @@ namespace OOP
     {
         static void Main(string[] args)
         {
+            Controller controller = new Controller();
+
+            Console.ReadLine();
+
+        }
+
+        internal void SetGarageDefaults()
+        {
+            /*
             Car car = new Car(name: "Жигули", weight: 1500, maxSpeed: 100, acceleration: 5, wheelDriveType: Vehicle.WheelDriveTypes.RearWhellDrive,
-                engineType: Vehicle.GroundVehicleEngineTypes.Gasoline, horsePower: 70, bodyType: Vehicle.CarBodyTypes.Sedan);
+    engineType: Vehicle.GroundVehicleEngineTypes.Gasoline, horsePower: 70, bodyType: Vehicle.CarBodyTypes.Sedan);
             car.DisplayVehicleInfo();
 
             Motorcycle moto = new Motorcycle(name: "Harley-Davidson", weight: 100, maxSpeed: 290, acceleration: 30,
@@ -32,9 +42,7 @@ namespace OOP
 
             Plane plane2 = new Plane(name: "F4U Corsair", weight: 2000, maxSpeed: 485, acceleration: 40, maxAltitude: 3500,
                 verticalAcceleration: 20, loadCapacity: 1500, Vehicle.AirVehicleEngineTypes.Radial, runwayLength: 350);
-            plane2.DisplayVehicleInfo();
-
-            Console.ReadLine();
+            plane2.DisplayVehicleInfo();*/
         }
 
 
